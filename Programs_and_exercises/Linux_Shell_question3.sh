e926b8d414579aebb72282303be0012c31ec039c
VisHnu Vijay <70762767+Vishnu-Vijay95@users.noreply.github.com>
2020-09-08 11:03:24 +0530

    Shell Script to Study the use of Pip utility

---
+++ Programs_and_exercises/Linux_Shell_question3.sh
@@ -0,0 +1,14 @@
+!/bin/bash
+touch a.txt
+chmod 777 a.txt
+touch b.txt
+chmod 777 b.txt
+vim a.txt
+cp a.txt b.txt
+vim b.txt
+diff a.txt b.txt
+diff a.txt b.txt > a.patch| patch a.txt a.patch | diff a.txt b.txt
+#patch a.txt a.patch
+vim a.txt
+rm a.txt
+rm b.txt
